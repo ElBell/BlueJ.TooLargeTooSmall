1fedc7267bffb35d9da2ebd8ada24e9cee597c0a
Eleonor Bart <eleonorbart@zipcoders-MacBook-Pro-7.local>
2019-02-05 15:06:08 -0500

    Solved the problem. Created a random number game that tells you if it's too high, too low, correct, or the same number as you just entered. Logs your number of attempts once correct

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 /**
  * Write a description of class Main here.
  *
@@ -7,16 +7,12 @@
  */
 public class Main
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
     /**
      * Constructor for objects of class Main
      */
     public Main()
     {
-        // initialise instance variables
-        x = 0;
+        
     }
 
     /**
@@ -25,9 +21,31 @@ public class Main
      * @param  y  a sample parameter for a method
      * @return    the sum of x and y
      */
-    public int sampleMethod(int y)
+    public void guessingGame()
     {
-        // put your code here
-        return x + y;
-    }
+      int attempts = 1;
+      int random = (int)(Math.random() * (10 - 1) + 1);
+      int guess; 
+      int lastGuess = 99;
+      Scanner guessInput = new Scanner(System.in);
+      System.out.println("Let's play a game! Guess a number between 1 and 10");
+      guess = guessInput.nextInt();
+      while (guess != random) {
+          if (lastGuess == guess) {
+              System.out.println("You just guessed that, silly");
+              guess = guessInput.nextInt();
+            } else {
+              if (guess > random) {
+                  System.out.println(guess + " is too high");
+              } else {
+                  System.out.println(guess + " is too low");
+              }
+              System.out.println("Guess again!");
+              lastGuess = guess;
+              guess = guessInput.nextInt();
+              attempts = attempts + 1;
+         } 
+        } 
+     System.out.println(guess + " is correct! It took you " + attempts + " guesses");
+}
 }
